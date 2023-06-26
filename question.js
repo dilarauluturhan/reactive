c523e21e0a0b97f3eb8f6bef384b7b91aa1b99a3
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-06-26 23:04:05 +0300

    feat: added questions

---
+++ question.js
@@ -0,0 +1,102 @@
+const quiz = [
+    {
+        question: 'In which programming language is React.js written?',
+        options: ['JavaScript', 'Python', 'Ruby', 'Java'],
+        answer: 0
+    },
+    {
+        question: 'How is data communication between React components accomplished?',
+        options: ['Props', 'State', 'Events', 'Refs'],
+        answer: 0
+    },
+    {
+        question: 'What is JSX in React.js?',
+        options: ['JavaScript XML', 'JavaScript Extension', 'JSX Script', 'JavaScript Syntax'],
+        answer: 0
+    },
+    {
+        question: 'Which method is called before the render function in the lifecycle of React components?',
+        options: ['componentDidMount', 'componentWillMount', 'componentDidUpdate', 'componentWillUnmount'],
+        answer: 1
+    },
+    {
+        question: 'What types of applications can you develop with React.js?',
+        options: ['Web applications', 'Mobile applications', 'Desktop applications', 'All of the above'],
+        answer: 3
+    },
+    {
+        question: 'Which JavaScript library or framework does React use?',
+        options: ['jQuery', 'Angular', 'Vue.js', 'None'],
+        answer: 3
+    },
+    {
+        question: 'How do you update the state of a React component?',
+        options: ['setState()', 'updateState()', 'changeState()', 'modifyState()'],
+        answer: 0
+    },
+    {
+        question: 'In React projects, which HTML tag is used to define the element where components will be placed?',
+        options: ['react', 'component', 'div', 'render'],
+        answer: 2
+    },
+    {
+        question: 'How do you style a component in React.js?',
+        options: ['Using a CSS file', 'Using inline style attributes', 'Controlling CSS classes', 'All of the above'],
+        answer: 3
+    },
+    {
+        question: 'What type of data flow model does React.js use?',
+        options: ['Unidirectional data flow', 'Bidirectional data flow', 'Multidirectional data flow', ' Asynchronous data flow'],
+        answer: 0
+    },
+    {
+        question: 'How do you pass a component to another component in React?',
+        options: ['Using Props', 'Using State', 'Using Refs', 'Using Context API'],
+        answer: 0
+    },
+    {
+        question: 'In the class-based structure of React components, what should the render() method return?',
+        options: ['A string', 'A number', 'An HTML element', 'It can be any data type'],
+        answer: 2
+    },
+    {
+        question: 'How do you add event listeners to React components?',
+        options: ['onClick', 'onChange', 'onSubmit', 'All of the above'],
+        answer: 3
+    },
+    {
+        question: 'What feature is used to make data requests in React.js?',
+        options: ['fetch()', 'axios()', 'ajax()', 'request()'],
+        answer: 0
+    },
+    {
+        question: 'How can you hide a component in React?',
+        options: ['By changing its visibility using CSS', 'By using display: none', 'By updating the state', 'By not rendering it'],
+        answer: 2
+    },
+    {
+        question: 'How do you modify the state of a component in React?',
+        options: ['this.state()', 'this.setState()', 'this.modifyState()', 'this.updateState()'],
+        answer: 1
+    },
+    {
+        question: 'How is the root component created in React?',
+        options: ['render()', 'createRoot()', 'mount()', 'createComponent()'],
+        answer: 1
+    },
+    {
+        question: 'How do you create a new component in React using which command?',
+        options: ['createComponent()', 'makeComponent()', 'createElement()', 'buildComponent()'],
+        answer: 3
+    },
+    {
+        question: 'In the class-based structure of React components, how do components connect to each other?',
+        options: ['Using props', 'Using state', 'Using refs', 'Using context'],
+        answer: 0
+    },
+    {
+        question: 'What concept is used in React.js to render components in the appropriate order?',
+        options: ['Virtual DOM', 'Real DOM', 'Shadow DOM', 'Component DOM'],
+        answer: 0
+    }
+]
