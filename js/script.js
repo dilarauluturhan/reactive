7ff30f3018b39b2957302de5cf9d5aa6b71d8021
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-06-29 01:37:27 +0300

    feat: added js comments

---
+++ js/script.js
@@ -0,0 +1,135 @@
+const questionText = document.querySelector('.question');
+const optionList = document.querySelector('.option_list');
+const option = document.querySelector('.option');
+const rulesBox = document.querySelector('.rules_box');
+const quizBox = document.querySelector('.quiz_box');
+const resultBox = document.querySelector('.results');
+
+let questionCounter = 0; // Kullanıcının kaçıncı soruda olduğunu takip etmek için.. Sorular ilerledikçe bu değer artar
+let currentQuestion; // Şu anda görüntülenen soruyu temsil eder, her soru gösterildiğinde bu değişken güncellenir
+let currentQuestionIndex = 0; // Şu anda hangi sorunun görüntülendiğini takip etmek için.. Soruların dizideki indeksini temsil eder
+let availableQuestions = []; // Bu dizi soruların listesini tutar, başlangıçta boştur ancak quiz başladığında sorular bu diziye eklenir
+let availableOptions = []; // Bu dizi şu anda görüntülenen sorunun seçeneklerini tutar her soru gösterildiğinde seçenekler bu diziye eklenir
+let correctAnswers = 0; // Doğru cevap sayısını takip etmek için.. Kullanıcı doğru cevap verdiğinde değer artar
+let attempt = 0; // Kullanıcının çözdüğü soru sayısını takip etmek için.. Kullanıcı her soru çözdüğünde bu değer artar
+
+// bu fonksiyon çağırıldığında quiz array'indeki tüm soruları availableQuestions'a ekleyerek soru listesi oluşturur
+function setAvailableQuestions() {
+    const totalQuestion = quiz.length;
+    for (let i = 0; i < totalQuestion; i++) {
+        availableQuestions.push(quiz[i])
+    }
+}
+
+// bu fonksiyon rastgele soru seçer, rastgele şık sıralar, şıkları günceller ve tıklanan şıkka göre sonucu render eden getResult'ı çağırır
+function getNewQuestion() {
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]; // rastgele soru gelmesi için rastgele bir indeks alır
+    currentQuestion = questionIndex; // seçilen soruyu currentQuestion'a atar
+    questionText.innerHTML = currentQuestion.question; // questionText'i currentQuestion'ın question'ıyla günceller
+    const index1 = availableQuestions.indexOf(questionIndex); // availableQuestions dizisinde seçilen sorunun indeksini bulur ve index1 değişkenine atar
+    availableQuestions.splice(index1, 1); // availableQuestions dizisinden seçilen soruyu çıkarır
+    const optionLength = currentQuestion.options.length; // Seçilen sorunun seçeneklerinin length'ini optionLength değişkenine atar
+    for (let i = 0; i < optionLength; i++) {
+        availableOptions.push(i); // availableOptions'a şıkların indeksini ekler
+    }
+
+    optionList.innerHTML = ''; // Seçenek listesini temizler
+
+    // her şık için for döngüsü..
+    for (let i = 0; i < optionLength; i++) {
+        const optionIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+        const index2 = availableOptions.indexOf(optionIndex);
+        availableOptions.splice(index2, 1);
+        const option = document.createElement("div");
+        option.innerHTML = currentQuestion.options[optionIndex]; // <div> öğesinin içeriğini currentQuestion'ın options'ındaki seçeneğe göre günceller.
+        option.id = optionIndex;
+        option.className = "option";
+        optionList.appendChild(option); // option'ı optionList'e ekler
+        option.setAttribute("onclick", "getResult(this)");
+    }
+
+    questionCounter++; // soru sayacını bir arttırır
+}
+
+/***********correct ve wrong'un css kodları çalışmıyo************/
+function getResult(element) {
+    const id = parseInt(element.id); // tıklanan şıkkın id'sini alır
+    if (id === currentQuestion.answer) {
+        element.classList.add("correct"); // tıklanan seçeneğin id'si doğru cevaba eşitse "correct" class'ını ekler
+        correctAnswers++;
+        // console.log("correct:" + correctAnswers)
+    } else {
+        element.classList.add("wrong"); // tıklanan seçeneğin id'si doğru cevaba eşit değilse "wrong" class'ını ekler
+        const optionLength = optionList.children.length;
+        // her şık için for döngüsü..
+        for (let i = 0; i < optionLength; i++) {
+            if (parseInt(optionList.children[i].id) === currentQuestion.answer) {
+                optionList.children[i].classList.add("correct"); // şıklardan herhangi biri sorunun doğru cevabıyla eşitse "correct" class'ını seçeneğe ekler
+            }
+        }
+    }
+    attempt++; // çözülen soru sayısı birer artar
+    unclickableOptions(); // şıklara bir kere tıkladıktan sonra diğerlerini tıklanamaz hale getirir
+}
+
+/*******alreadyAnswered'ın css kodları çalışmıyo********/
+function unclickableOptions() {
+    const optionLength = optionList.children.length; // şıkların sayısını optionLength'e atar
+    for (let i = 0; i < optionLength; i++) {
+        optionList.children[i].classList.add("alreadyAnswered")
+    }
+}
+
+/*******************************************************/
+function back() {
+    console.log("back!");
+    if (currentQuestionIndex <= 0) {
+        return; // geri gidilecek bir önceki soru yok
+    }
+
+    currentQuestionIndex--;
+    getNewQuestion();
+}
+/*******************************************************/
+
+// bu fonksiyon tüm sorular cevaplandıysa quizOver()'ı bir sonraki soruyu almak içinse getNewQuestion()'ı çağırır
+function next() {
+    if (questionCounter == quiz.length) {
+        quizOver();
+    } else {
+        getNewQuestion();
+    }
+}
+
+function quizOver() {
+    quizBox.classList.add("hide");
+    resultBox.classList.remove("hide");
+    quizResult();
+}
+
+function quizResult() {
+    resultBox.querySelector(".totalQuestion").innerHTML = quiz.length;
+    resultBox.querySelector(".totalAttempt").innerHTML = attempt;
+    resultBox.querySelector(".totalCorrect").innerHTML = correctAnswers;
+    resultBox.querySelector(".totalWrong").innerHTML = attempt - correctAnswers;
+    resultBox.querySelector(".totalScore").innerHTML = correctAnswers + " / " + quiz.length;
+}
+
+function resetQuiz() {
+    questionCounter = 0;
+    correctAnswers = 0;
+    attempt = 0;
+}
+
+function quitButton() {
+    resultBox.classList.add("hide");
+    rulesBox.classList.remove("hide");
+    resetQuiz();
+}
+
+function start() {
+    rulesBox.classList.add("hide");
+    quizBox.classList.remove("hide");
+    setAvailableQuestions(); // soru listesi için..
+    getNewQuestion(); // yeni soru getirmek için..
+}
